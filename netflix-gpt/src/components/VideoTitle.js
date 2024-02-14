9228d6f724226c53b940d8de7cf25de9aadb070e
PRAJAKTA SELUKAR <34501121+PrajaktaSelukar@users.noreply.github.com>
2024-02-14 18:19:23 +0530

    Build Video Title

--- netflix-gpt/src/components/VideoTitle.js
+++ netflix-gpt/src/components/VideoTitle.js
@@ -4,8 +4,8 @@ const VideoTitle = ({ title, overview }) => {
         <h1 className="text-6xl font-bold">{ title }</h1>
         <p className="py-6 text-lg w-1/4">{ overview }</p>
         <div>
-            <button className="bg-white text-black py-4 px-12 text-xl opacity-50 rounded-lg">▶️ Play</button>
-            <button className="bg-gray-500 text-white mx-2 py-4 px-12 text-xl opacity-50 rounded-lg">More Info</button>
+            <button className="bg-white text-black py-4 px-12 text-xl opacity-80 rounded-lg">▶️ Play</button>
+            <button className="bg-gray-500 text-white mx-2 py-4 px-12 text-xl opacity-80 rounded-lg">More Info</button>
         </div>
     </div>
   )
